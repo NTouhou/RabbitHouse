929390b3de73345577e7dd992080f793ac207275
shingo1998 <shingo_1998@ezweb.ne.jp>
2018-11-22 11:43:36 +0900

    下移動

--- SCR2-9/pingu.java
+++ SCR2-9/pingu.java
@@ -15,5 +15,10 @@ public class pingu extends Actor
     public void act() 
     {
         // Add your action code here.
+        if( Greenfoot.isKeyDown( "down" ) ){
+        setRotation(90);
+        move(1);
+}
+
     }    
 }
