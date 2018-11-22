1468c550811210f6e024a540adda4f83866573f8
NTouhou <naoki.touhou@gmail.com>
2018-11-22 11:36:04 +0900

    上移動を追加2

--- SCR2-9/pingu.java
+++ SCR2-9/pingu.java
@@ -14,6 +14,9 @@ public class pingu extends Actor
      */
     public void act() 
     {
-        // Add your action code here.
+        if( Greenfoot.isKeyDown( "up" ) ){
+                setRotation(270);
+                move(3);
+        }
     }    
 }
